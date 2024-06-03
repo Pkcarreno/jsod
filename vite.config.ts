b769360aa96a2704f28bff0899c2d39d82738df3
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-06-02 22:34:15 -0400

    fix: path to icons

--- vite.config.ts
+++ vite.config.ts
@@ -27,25 +27,25 @@ export default defineConfig(({ mode }) => {
           description: 'Write, run, and share JavaScript code instantly.',
           icons: [
             {
-              src: '/pwa-192x192.png',
+              src: `${process.env.BASE_URL}/pwa-192x192.png`,
               sizes: '192x192',
               type: 'image/png',
               purpose: 'any',
             },
             {
-              src: '/pwa-512x512.png',
+              src: `${process.env.BASE_URL}/pwa-512x512.png`,
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any',
             },
             {
-              src: '/pwa-maskable-192x192.png',
+              src: `${process.env.BASE_URL}/pwa-maskable-192x192.png`,
               sizes: '192x192',
               type: 'image/png',
               purpose: 'maskable',
             },
             {
-              src: '/pwa-maskable-512x512.png',
+              src: `${process.env.BASE_URL}/pwa-maskable-512x512.png`,
               sizes: '512x512',
               type: 'image/png',
               purpose: 'maskable',
