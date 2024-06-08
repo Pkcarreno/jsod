02dcefdf5c84614bd1585b80223b02a108840cf5
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2024-06-08 14:31:18 -0400

    feat: show badge on bottom bar in mobile view
    when there are new logs on the output screen, it displays a badge as an alert for the user in
    mobille view

--- src/features/editor/stores/editor/logs-store.ts
+++ src/features/editor/stores/editor/logs-store.ts
@@ -10,23 +10,30 @@ const logsList = new LinkedLogs();
 interface LogsState {
   logs: log[];
   isFirstTime: boolean;
+  alert: boolean;
   appendLogs: (logs: log) => void;
   clearLogs: () => void;
+  clearAlert: () => void;
 }
 
 const _useLogsStore = create<LogsState>((set) => ({
   logs: [],
   isFirstTime: true,
+  alert: false,
   appendLogs: (log: log) => {
     logsList.append(log);
     set({
       logs: logsList.getAllLogsInArray(),
       isFirstTime: false,
+      alert: true,
     });
   },
   clearLogs: () => {
     logsList.clearFirst();
     set({ logs: [] });
+  },
+  clearAlert: () => {
+    set({ alert: false });
   },
 }));
 
